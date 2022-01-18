aa02ec5d957666bd548e3c6580357e71307e03d7
Joe <hutcheson2001@yahoo.com>
2022-01-18 11:16:37 -0500

    Help from Ryan

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(array, name){
+    let match = array.filter(element => element.toUpperCase() === name.toUpperCase())
+    return match
+  }
+
+  function fuzzyMatch(array, string){
+    const stringArray = Object.assign([], string)
+     
+     for (let item of array){
+       let nameObject = Object.assign([],item.name)
+      let firstLetters = nameObject.slice(0,2)
+      let firstArrayLetters = stringArray.slice(0,2)
+       console.log(firstLetters)
+       console.log(firstArrayLetters)
+       if (firstLetters === firstArrayLetters){
+      return item
+     }
+    }
+    
+  }
+  
